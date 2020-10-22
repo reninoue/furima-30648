303f5bff7e9e036def27a99b5f4d3c74b3ef34d7
reninoue <71805701+reninoue@users.noreply.github.com>
2020-10-22 20:26:07 +0900

    モデルの単体テスト及ハンドリングエラーの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,21 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
   validates :birth_date, presence: true
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, :password_confirmation, with: PASSWORD_REGEX, message: 'include both letters and numbers' 
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
+    validates :first_name
+    validates :last_name
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message: '全角(カタカナ)を使用してください' } do
+    validates :first_name_kana
+    validates :last_name_kana
+  end
+  
 
   has_many :items
   has_many :purchases
---
+++ spec/system/users_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe 'ユーザー新規登録', type: :system do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+  
+    
+end
