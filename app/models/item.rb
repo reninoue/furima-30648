dfc599ef69d9e6c951a5cffe0b5fd6b1aa1bfe5e
reninoue <71805701+reninoue@users.noreply.github.com>
2020-10-26 19:11:30 +0900

    コードレビュー修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,16 +7,20 @@ class Item < ApplicationRecord
   belongs_to_active_hash :handling_time
 
   belongs_to :user
-  has_one_attached :image
   has_one :purchase
 
-  validates :user, presence: true
-  validates :name, presence: true, length: { maximum: 40 }
-  validates :description, presence: true, length: { maximum: 1000 }
-  validates :category_id, presence: true, numericality: { other_than: 1 }
-  validates :condition_id, presence: true, numericality: { other_than: 1 }
-  validates :postage_payer_id, presence: true, numericality: { other_than: 1 }
-  validates :prefecture_id, presence: true, numericality: { other_than: 0 }
-  validates :handling_time_id, presence: true, numericality: { other_than: 1 }
-  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  has_one_attached :image
+  validates :image, presence: true
+
+  with_options presence: true do
+    validates :user
+    validates :name, length: { maximum: 40 }
+    validates :description, length: { maximum: 1000 }
+    validates :category_id, numericality: { other_than: 1 }
+    validates :condition_id, numericality: { other_than: 1 }
+    validates :postage_payer_id, numericality: { other_than: 1 }
+    validates :prefecture_id, numericality: { other_than: 0 }
+    validates :handling_time_id, numericality: { other_than: 1 }
+    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  end
 end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,13 +2,12 @@ FactoryBot.define do
   factory :item do
     name              { '名前' }
     description       { '説明' }
-    image             { Faker::Lorem.sentence }
     category_id       { 2 }
     condition_id      { 2 }
     postage_payer_id  { 2 }
     prefecture_id     { 2 }
     handling_time_id  { 2 }
     price             { 1111 }
-    user              { FactoryBot.create(:user) }
+    association :user
   end
 end
